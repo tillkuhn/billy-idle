174d574e079c8c23d55cf72fac20d2e2b030a646
Till Kuhn <till.kuhn@dbschenker.com>
2024-09-27 17:01:35 +0200

    add signal handling

--- main.go
+++ main.go
@@ -3,48 +3,61 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
+	"os/signal"
 	"regexp"
 	"strconv"
+	"syscall"
 	"time"
 )
 
 const (
-	interval  = 1 * time.Second
-	idleAfter = 5 * time.Second
+	interval   = 1 * time.Second
+	idleAfter  = 5 * time.Second
+	dateLayout = time.RFC1123
 )
 
 var idleMatcher = regexp.MustCompile("\"HIDIdleTime\"\\s*=\\s*(\\d+)")
 
 // main runs the tracker
 func main() {
-	started := time.Now()
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGHUP)
+	busyStart := time.Now()
 	idle := false
-	fmt.Printf("🐝 Starting in busy mode at %v\n", time.Now().Format(time.Kitchen))
-	for {
-		cmd := exec.Command("ioreg", "-c", "IOHIDSystem")
-		stdout, err := cmd.Output()
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-
-		match := idleMatcher.FindStringSubmatch(string(stdout))
-		var idleMillis int64
-		if match != nil {
-			if i, err := strconv.Atoi(match[1]); err == nil {
-				idleMillis = int64(i / 1000000)
+	fmt.Printf("🐝 Starting in busy mode at %v\n", time.Now().Format(dateLayout))
+	go func() {
+		for {
+			cmd := exec.Command("ioreg", "-c", "IOHIDSystem")
+			stdout, err := cmd.Output()
+			if err != nil {
+				log.Fatal(err.Error())
 			}
-		} else {
-			log.Fatal("Can't parse HIDIdleTime from output")
-		}
 
-		if idle == false && idleMillis >= idleAfter.Milliseconds() {
-			idle = true
-			fmt.Printf("💤 Switched to idle after %ds idle time (running since %v)\n", idleMillis/1000, time.Now().Sub(started).Round(time.Second))
-		} else if idle == true && idleMillis < idleAfter.Milliseconds() {
-			idle = false
-			fmt.Printf("🐝 Switched to busy (running since %v)\n", time.Now().Sub(started).Round(time.Second))
+			match := idleMatcher.FindStringSubmatch(string(stdout))
+			var idleMillis int64
+			if match != nil {
+				if i, err := strconv.Atoi(match[1]); err == nil {
+					idleMillis = int64(i / 1000000)
+				}
+			} else {
+				log.Fatal("Can't parse HIDIdleTime from output")
+			}
+
+			if idle == false && idleMillis >= idleAfter.Milliseconds() {
+				idle = true
+				fmt.Printf("💤 Switched to idle after %ds idle and %v busy time at %v\n",
+					idleMillis/1000, time.Now().Sub(busyStart).Round(time.Second), time.Now().Format(dateLayout))
+			} else if idle == true && idleMillis < idleAfter.Milliseconds() {
+				idle = false
+				busyStart = time.Now()
+				fmt.Printf("🐝 Switched to busy mode at %s\n", time.Now().Format(dateLayout))
+			}
+			time.Sleep(interval)
 		}
-		time.Sleep(interval)
-	}
+	}()
+	<-c
+	fmt.Printf("🐝 Stopped at %v\n", time.Now().Format(dateLayout))
+
 }
