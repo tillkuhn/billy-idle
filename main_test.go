0becf8e411c755e80f2ae76784b019edcf5a804c
Till Kuhn <tillkuhn@users.noreply.github.com>
2024-09-27 17:24:30 +0200

    add test

--- main_test.go
+++ main_test.go
@@ -9,6 +9,7 @@ import (
 func Test_Tracker(t *testing.T) {
 	checkInterval = 100 * time.Millisecond
 	idleAfter = 200 * time.Millisecond
+	cmd = "testdata/ioreg-mock.sh"
 	go func() { main() }()
 	time.Sleep(500 * time.Millisecond)
 	c <- os.Interrupt
