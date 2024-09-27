830236663a7f4a57ff18833384c8732bbf1df314
Till Kuhn <till.kuhn@dbschenker.com>
2024-09-27 19:51:28 +0200

    1st refactoring

--- main.go
+++ main.go
@@ -1,66 +1,157 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"os/signal"
+	"path/filepath"
 	"regexp"
 	"strconv"
 	"syscall"
 	"time"
+
+	_ "modernc.org/sqlite"
 )
 
 const (
-	dateLayout = time.RFC1123
+	dateLayout     = time.RFC1123
+	recreateSchema = true // CAUTION !!!!
 )
 
 var (
-	checkInterval = 1 * time.Second
-	idleAfter     = 5 * time.Second
-	idleMatcher   = regexp.MustCompile("\"HIDIdleTime\"\\s*=\\s*(\\d+)")
 	c             = make(chan os.Signal, 1)
 	cmd           = "ioreg"
+	clientID      = "default"
+	dbDirectory   = "./sqlite"
+	checkInterval = 1 * time.Second
+	idleAfter     = 3 * time.Second
+	idleMatcher   = regexp.MustCompile("\"HIDIdleTime\"\\s*=\\s*(\\d+)")
 )
 
 // main runs the tracker
 func main() {
+	ctx, ctxCancel := context.WithCancel(context.Background())
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGHUP)
-	busyStart := time.Now()
-	idle := false
-	fmt.Printf("🐝 Starting in busy mode at %v\n", time.Now().Format(dateLayout))
+
+	db, err := initDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(db *sql.DB) { _ = db.Close() }(db)
 	go func() {
+		idle := false
+		lastEvent := time.Now()
+
+		id, _ := insertTrack(ctx, db, fmt.Sprintf("🐝 Start tracking in busy mode, idle time kicks in after %vs", idleAfter.Seconds()))
 		for {
-			cmd := exec.Command(cmd, "-c", "IOHIDSystem")
-			stdout, err := cmd.Output()
-			if err != nil {
-				log.Fatal(err.Error())
-			}
-
-			match := idleMatcher.FindStringSubmatch(string(stdout))
-			var idleMillis int64
-			if match != nil {
-				if i, err := strconv.Atoi(match[1]); err == nil {
-					idleMillis = int64(i / 1000000)
-				}
-			} else {
-				log.Fatal("Can't parse HIDIdleTime from output" + string(stdout))
-			}
-
-			if !idle && idleMillis >= idleAfter.Milliseconds() {
+			idleMillis, err := currentIdleTime(ctx)
+			switch {
+			case err != nil:
+				log.Println(err.Error())
+			case !idle && idleMillis >= idleAfter.Milliseconds():
 				idle = true
-				fmt.Printf("💤 Switched to idle after %ds idle and %v busy time at %v\n",
-					idleMillis/1000, time.Since(busyStart).Round(time.Second), time.Now().Format(dateLayout))
-			} else if idle && idleMillis < idleAfter.Milliseconds() {
+				if err := completeTrack(ctx, db, id); err != nil {
+					log.Println(err.Error())
+				}
+				log.Printf("[%s] 💤 Switched to idle mode after %v of busy time, completing record #%d\n",
+					clientID, time.Since(lastEvent).Round(time.Second), id)
+				lastEvent = time.Now()
+			case idle && idleMillis < idleAfter.Milliseconds():
 				idle = false
-				busyStart = time.Now()
-				fmt.Printf("🐝 Switched to busy mode at %s\n", time.Now().Format(dateLayout))
+				id, err = insertTrack(ctx, db, fmt.Sprintf("🐝 Resuming busy mode after %v of idle time, creating new record", time.Since(lastEvent).Round(time.Second)))
+				if err != nil {
+					log.Println(err.Error())
+				}
+				lastEvent = time.Now()
 			}
 			time.Sleep(checkInterval)
 		}
 	}()
 	<-c
-	fmt.Printf("🐝 Stopped at %v\n", time.Now().Format(dateLayout))
+	ctxCancel()
+	log.Printf("🐝 Stopped at %v\n", time.Now().Format(dateLayout))
+}
 
+// currentIdleTime gets the current idle time in milliseconds from the external ioreg command
+func currentIdleTime(ctx context.Context) (int64, error) {
+	cmd := exec.CommandContext(ctx, cmd, "-c", "IOHIDSystem")
+	stdout, err := cmd.Output()
+	if err != nil {
+		return 0, err
+	}
+
+	match := idleMatcher.FindStringSubmatch(string(stdout))
+	var t int64
+	if match != nil {
+		if i, err := strconv.Atoi(match[1]); err == nil {
+			t = int64(i) / time.Second.Microseconds()
+		}
+	} else {
+		return t, fmt.Errorf("%w can't parse HIDIdleTime from output %s", err, string(stdout))
+	}
+	return t, nil
+}
+
+// initDB initializes SQLite DB in local filesystem
+func initDB() (*sql.DB, error) {
+	fn := filepath.Join(dbDirectory, "db")
+
+	db, err := sql.Open("sqlite", fn)
+	if err != nil {
+		return nil, err
+	}
+
+	clientID, err = os.Hostname()
+	if err != nil {
+		return nil, err
+	}
+
+	// drop table if exists t; insert into t values(42), (314);
+	var dropStmt string
+	if recreateSchema {
+		dropStmt = "DROP TABLE IF EXISTS track;\n"
+	}
+	if _, err = db.Exec(dropStmt + `
+CREATE TABLE IF NOT EXISTS track (
+		"id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+		"busy_start" DATETIME NOT NULL DEFAULT (datetime(CURRENT_TIMESTAMP, 'localtime')), 
+		"busy_end" DATETIME,
+		"client" TEXT,
+		"message" TEXT)
+`); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+// insertTrack inserts a new tracking records
+func insertTrack(ctx context.Context, db *sql.DB, msg string) (int, error) {
+	log.Printf("[%s] %s", clientID, msg)
+	statement, err := db.PrepareContext(ctx, `INSERT INTO track(message,client) VALUES (?,?) RETURNING id;`)
+	if err != nil {
+		return 0, err
+	}
+	var id int
+	// Golang SQL insert row and get returning ID example: https://gist.github.com/miguelmota/d54814683346c4c98cec432cf99506c0
+	err = statement.QueryRowContext(ctx, msg, clientID).Scan(&id)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	return id, err
+}
+
+// completeTrack completes an existing tracking record by setting the busy_end date
+func completeTrack(ctx context.Context, db *sql.DB, id int) error {
+	statement, err := db.PrepareContext(ctx, `UPDATE track set busy_end=datetime(CURRENT_TIMESTAMP, 'localtime') WHERE id=(?)`)
+	if err != nil {
+		return err
+	}
+	// Golang SQL insert row and get returning ID example: https://gist.github.com/miguelmota/d54814683346c4c98cec432cf99506c0
+	_, err = statement.ExecContext(ctx, id)
+	return err
 }
