9ba56b6f4d32e286933c9ec69edd45a9e8cdfec1
Till Kuhn <tillkuhn@users.noreply.github.com>
2024-11-18 19:19:21 +0100

    test output

--- pkg/tracker/db.go
+++ pkg/tracker/db.go
@@ -21,7 +21,11 @@ var initSQL string
 
 // initDB initializes SQLite DB in local filesystem
 func initDB(opts *Options) (*sqlx.DB, error) {
+	if err := os.MkdirAll(opts.AppDir(), os.ModePerm); err != nil {
+		return nil, fmt.Errorf("cannot create app dir %s: %w", opts.AppDir(), err)
+	}
 	dbFile := filepath.Join(opts.AppDir(), "db.sqlite3")
+
 	log.Printf("🥫 Open database file=%s sqlite=%s", dbFile, sqlite3.SQLITE_VERSION)
 	db, err := sqlx.Open("sqlite", dbFile)
 	if err != nil {
