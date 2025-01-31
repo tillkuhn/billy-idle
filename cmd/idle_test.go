01e7d1787baf9a86bd790871d2a1033f9cdd26f6
Till Kuhn <till.kuhn@dbschenker.com>
2025-01-31 15:21:32 +0100

    basic support for 'idle' subcommand in gRPC

--- cmd/idle_test.go
+++ cmd/idle_test.go
@@ -10,14 +10,14 @@ import (
 	"github.com/tillkuhn/billy-idle/pkg/tracker"
 )
 
-// Todo align with Idle test, should be less redundant
-func TestWSPStatusError(t *testing.T) {
-	var grpcTestPort = 50052 // use different port for test to avoid conflicts
+// Todo align with WSP test, should be less redundant
+func TestIdle(t *testing.T) {
+	var grpcTestPort = 50054 // use different port for each test to avoid conflicts
 	actual := new(bytes.Buffer)
 	rootCmd.SetOut(actual)
 	rootCmd.SetErr(actual)
 	wspArgs := []string{"--port", strconv.Itoa(grpcTestPort)}
-	rootCmd.SetArgs(slices.Insert(wspArgs, 0, wspCmd.Use))
+	rootCmd.SetArgs(slices.Insert(wspArgs, 0, idleCmd.Use))
 	// Returns "Error: rpc error: code = DeadlineExceeded desc = context deadline exceeded\nUsage:\n  b
 	// if no server
 	opts := &tracker.Options{
@@ -35,5 +35,5 @@ func TestWSPStatusError(t *testing.T) {
 	// assert.NoError(t, tr.ServeGRCP())
 	err := rootCmd.Execute()
 	assert.NoError(t, err)
-	assert.Contains(t, actual.String(), "state=busy")
+	assert.Contains(t, actual.String(), "idle until")
 }
