32d7635589c4a8644174e1ff12aa96804a66227f
Till Kuhn <tillkuhn@users.noreply.github.com>
2026-01-27 18:10:28 +0100

    add internal defaults

--- cmd/punch.go
+++ cmd/punch.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/spf13/cobra"
+	"github.com/tillkuhn/billy-idle/internal/defaults"
 	"github.com/tillkuhn/billy-idle/pkg/tracker"
 )
 
--- cmd/report.go
+++ cmd/report.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/fatih/color"
+	"github.com/tillkuhn/billy-idle/internal/defaults"
 
 	"github.com/tillkuhn/billy-idle/pkg/tracker"
 
